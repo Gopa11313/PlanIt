831eef56668c6eddb614898e75678feda4f6eeb2
gopa11313 <gopalaaa9819525993@gmail.com>
2023-09-25 18:51:45 -0400

    starting code

--- screens/onboarding/SpalshScreen.js
+++ screens/onboarding/SpalshScreen.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { StyleSheet, Text, SafeAreaView, View } from "react-native";
 export default function SplashScreen({ navigation, route }) {
   useEffect(() => {
+    //gopal's work
     navigation.navigate("WelcomePage");
   }, 4000);
   return (
