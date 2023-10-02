ee2a5c763e0611447c8306ef8e87a6689c8c9b72
gopa11313 <gopalaaa9819525993@gmail.com>
2023-10-02 19:27:15 -0400

    second sprint final

--- screens/onboarding/SignUp/SignUpScreen.js
+++ screens/onboarding/SignUp/SignUpScreen.js
@@ -1,12 +1,31 @@
 import { useEffect } from "react";
 import { StatusBar, Text } from "react-native";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
 export default function SignUpScreen({ navigation, route }) {
+  const handleSignUp = () => {
+    navigation.navigate("Dashboard");
+  };
+
+  const gotoLogin = () => {
+    navigation.navigate("Login");
+  };
   useEffect(() => {}, []);
   return (
     <SafeAreaView style={style.contianer}>
-      <Text>Sign up page</Text>
+      <TextInput placeholder="Full Name" />
+      <TextInput placeholder="Email" />
+      <TextInput placeholder="Username" />
+      <TextInput placeholder="Password" />
+      <TextInput placeholder="Re-Enter Password" />
+      <Pressable onPress={handleSignUp}>
+        <Text>Sign Up</Text>
+      </Pressable>
+
+      <Text>Already a user?</Text>
+      <Pressable onPress={gotoLogin}>
+        <Text>Login</Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
