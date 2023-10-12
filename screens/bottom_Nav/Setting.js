ac19c7a70b25835284ebd2261cf2abfad3b68349
Gopa11313 <gopalaaa9819525993@gmail.com>
2023-10-12 18:18:56 -0400

    setting page

--- screens/bottom_Nav/Setting.js
+++ screens/bottom_Nav/Setting.js
@@ -1,50 +1,137 @@
 import React from "react";
-import { Image } from "react-native";
-import { StyleSheet } from "react-native";
-import { Text, View, SafeAreaView } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  StatusBar,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
+import { TextInput } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Gurkirat from "../../assets/gurkirat.png";
-import { ScrollView } from "react-native";
-import GurkiSecond from "../../assets/gurkiSecond.png";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+
 export default function Settings() {
   return (
     <SafeAreaView style={style.container}>
-      <ScrollView style={style.scrollView}>
-        <View style={style.insideView}>
-          <Image source={Gurkirat} style={style.profileImage} />
-          <Text style={style.name}>Gurkirat, JT</Text>
-          <Image style={style.image} source={GurkiSecond} />
+      <View style={style.insideView}>
+        <View>
+          <Text style={style.profileName}>Profile</Text>
         </View>
-      </ScrollView>
+
+        <View>
+          <Image style={style.dp} source={Gurkirat} />
+        </View>
+
+        <View style={style.subContainers}>
+          <Text style={style.containerHeader}>General</Text>
+          <Text style={style.subContainersContent}>Gurkirat Jaitla</Text>
+          <Text style={style.subContainersContent}>gurkirat@gmail.com</Text>
+          <Text style={style.subContainersContent}>647-897-xxxx</Text>
+        </View>
+
+        <View style={style.subContainers}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              margin: 3,
+            }}
+          >
+            <Text style={style.containerHeader}>About Me</Text>
+            <MaterialIcons name="edit" size={24} color="black" />
+          </View>
+          <Pressable>
+            <Text style={style.subContainersContent}>Interest</Text>
+            {/* <AntDesign name="arrowright" size={24} color="black" /> */}
+          </Pressable>
+          <Pressable>
+            <Text style={style.subContainersContent}>Age</Text>
+          </Pressable>
+          <Pressable>
+            <Text style={style.subContainersContent}>Location</Text>
+          </Pressable>
+          <Pressable>
+            <Text style={style.subContainersContent}>Gender</Text>
+          </Pressable>
+        </View>
+
+        <View>
+          <Pressable style={style.logoutButton}>
+            <Text style={style.buttonText}>Logout</Text>
+          </Pressable>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
+
 const style = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    justifyContent: "top",
-    alignItems: "center",
-    paddingHorizontal: 20,
-    backgroundColor: "#F0F0F0",
-  },
-  scrollView: {
-    width: "100%",
+    backgroundColor: "#fff",
   },
   insideView: {
-    padding: 20,
-    width: "100%",
     alignItems: "center",
-  },
-  profileImage: { width: 150, height: 150, borderRadius: 200 / 2 },
-  name: {
-    fontSize: 22,
-    margin: 10,
-    fontWeight: "500",
-  },
-  image: {
+    padding: 10,
     width: "100%",
-    marginTop: 15,
-    height: 250,
-    borderRadius: 10,
+  },
+  profileName: {
+    fontSize: 25,
+    margin: 10,
+    fontWeight: "bold",
+    marginStart: 25,
+    color: "black",
+  },
+
+  dp: {
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+    marginBottom: 20,
+  },
+  containerHeader: {
+    fontSize: 18,
+    margin: 7,
+    fontWeight: "bold",
+    marginStart: 25,
+    color: "black",
+  },
+  subContainersContent: {
+    fontSize: 15,
+    margin: 7,
+    marginStart: 25,
+    color: "black",
+  },
+  subContainers: {
+    justifyContent: "center",
+    width: "85%",
+    marginBottom: 30,
+    borderRadius: 20,
+    backgroundColor: "white", // Set a background color
+    elevation: 4, // Add a soft shadow
+    shadowColor: "#000", // Shadow color
+    shadowOffset: { width: 2, height: 4 }, // Shadow offset
+    shadowOpacity: 1, // Shadow opacity
+    shadowRadius: 4, // Shadow radius
+    borderWidth: 1,
+  },
+
+  logoutButton: {
+    width: 200,
+    height: 50,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    // marginTop: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
