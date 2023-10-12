f31d358b3f251f4e98ec050a552f68ebaf270751
Gopa11313 <gopalaaa9819525993@gmail.com>
2023-10-11 20:09:42 -0400

    working on setting page

--- screens/bottom_Nav/Setting.js
+++ screens/bottom_Nav/Setting.js
@@ -1,9 +1,50 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image } from "react-native";
+import { StyleSheet } from "react-native";
+import { Text, View, SafeAreaView } from "react-native";
+import Gurkirat from "../../assets/gurkirat.png";
+import { ScrollView } from "react-native";
+import GurkiSecond from "../../assets/gurkiSecond.png";
 export default function Settings() {
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <SafeAreaView style={style.container}>
+      <ScrollView style={style.scrollView}>
+        <View style={style.insideView}>
+          <Image source={Gurkirat} style={style.profileImage} />
+          <Text style={style.name}>Gurkirat, JT</Text>
+          <Image style={style.image} source={GurkiSecond} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    justifyContent: "top",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "#F0F0F0",
+  },
+  scrollView: {
+    width: "100%",
+  },
+  insideView: {
+    padding: 20,
+    width: "100%",
+    alignItems: "center",
+  },
+  profileImage: { width: 150, height: 150, borderRadius: 200 / 2 },
+  name: {
+    fontSize: 22,
+    margin: 10,
+    fontWeight: "500",
+  },
+  image: {
+    width: "100%",
+    marginTop: 15,
+    height: 250,
+    borderRadius: 10,
+  },
+});
